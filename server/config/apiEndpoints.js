eaba4ebef14bc583d63bfd23eab5b9bea1452c8f
swFang <33407178+swFang@users.noreply.github.com>
2019-10-14 17:01:08 -0700

    Added in formatPlacesForApiCall
    Added in this function to allow myself to formate the correct string for api calls to the distanceMatrix api by google. As the max destinations+origins can only equal to 25, to minimize api calls, I want to minimize amount of calls, so i split the matrix into 5 and 20, so total of 4 calls to populate a 20 by 20 matrix of all destinations.

--- server/config/apiEndpoints.js
+++ server/config/apiEndpoints.js
@@ -1,5 +1,6 @@
 module.exports = { 
     geoCodeURL: 'https://maps.googleapis.com/maps/api/geocode/json',
     placesNearby: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-    textSearch: 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+    textSearch: 'https://maps.googleapis.com/maps/api/place/textsearch/json?',
+    distanceMatrix: 'https://maps.googleapis.com/maps/api/distancematrix/json?'
 }
--- server/utils/sortAttractions.js
+++ server/utils/sortAttractions.js
@@ -1,7 +1,17 @@
+const axios = require('axios');
+const apiEndpoint = require('../config/apiEndpoints');
+const keys = require('../config/keys');
+
+const API_PREFIX_PLACES_ID = 'place_id:'
+const distanceMatrix = apiEndpoint.distanceMatrix;
+
 function sortAttractions(attractions) { 
     if(attractions != null){
-      console.log(attractions);
-      return null;
+        var destinations = formatPlacesIdForApiCall(attractions);
+        var origins = formatPlacesIdForApiCall(attractions, 5);
+        getDistances(attractions, destinations, origins);
+        //getDistances(attractions, formatPlacesIdForApiCall(attractions));
+        return null;
     } 
     const noAttractions = Error('Attractions list was not populated at sortAttraction.js');
     throw noAttractions;
@@ -14,7 +24,61 @@ function createGraph(attractions) {
     });
 }
 
-function getDIstances(attractions) {
+function getDistances(attractions, formattedApiString, formattedApiOrigins) {
+    // var curidx = 0; 
+    // for(var i = 0; i < attractions.length; i += 5) {
+    //     axios.get(distanceMatrix, {
+    //         params: {
+    //             units: 'metric',
+    //             origins: formattedApiOrigins[curidx],
+    //             destinations: formattedApiString[0],
+    //             key: keys.googleAPI
+    //         }
+    //     }).then((response)=> {
+    //         console.log(response.data);
+    //     }).catch((error) => {
+    //         console.log(error);
+    //     });
+    //     curidx ++; 
+    // }
+    axios.get(distanceMatrix, {
+        params: {
+            units: 'metric',
+            origins: formattedApiOrigins[0],
+            destinations: formattedApiString[0],
+            key: keys.googleAPI
+        }
+    }).then((response)=> {
+        console.log(response.data);
+        response.data.rows.map(x => console.log(x));
+    }).catch((error) => {
+        console.log(error);
+    });
 
+}
+
+function formatPlacesIdForApiCall(attractions, group = attractions.length) {
+    var curIndex = 0; 
+    var apiArray = [];
+    var apiString = ''
+
+    for( var i = 0; i < attractions.length; i++ ) { 
+        if(curIndex < group) {
+            curIndex ++; 
+        } else { 
+            curIndex = 1; 
+            apiString = '';
+        }
+
+        apiString = apiString + API_PREFIX_PLACES_ID + attractions[i].place_id; 
+        if(curIndex != group) {
+            apiString = apiString + '|';
+        }
+
+        if(curIndex + 1 > group) {
+            apiArray.push(apiString);
+        }
+    }
+    return apiArray; 
 }
 module.exports = sortAttractions;
